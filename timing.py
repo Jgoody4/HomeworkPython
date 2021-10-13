dc8a4863002046ec528f5e57eba0938edeb141ce
Justin Goodman <justin.goodman@sjsu.edu>
2021-10-13 14:35:21 -0700

    Justin Goodman, fixed up timing.py

--- timing.py
+++ timing.py
@@ -2,11 +2,20 @@
 import time
 # Create decorator
 def calculate_time(func):
+	"""
+	This function will be used by the wrapper, hence it is the decorator.
+	"""
 	def wrapper():
+		"""
+		This function will save and print the time.
+		-------------------------------------------
+		Returns:
+		--------
+		wrapper = 
+		"""
 		start = time.time()
 		func()
 		finish = time.time()
-		print("Total time ", finish - start)
+		X = finish - start
+		print(f'Total time {X}')
 	return wrapper
-calculate_time()
-
