468e8b018de19aafd1ca917125cf2d117268704d
Justin Goodman <justin.goodman@sjsu.edu>
2021-10-14 17:17:46 -0700

    This is my final version for all parts of the Python Homework. Everything is up to date with docstrings, comments, and a final readme file.

---
+++ README.md
@@ -0,0 +1,5 @@
+Justin Goodman
+015327104
+Professor Rojas
+Homework Python
+github repository link : https://github.com/Jgoody4/HomeworkPython.git
--- triple.py
+++ triple.py
@@ -1,10 +1,20 @@
 def tripler(func):
+	"""
+	This function is the decorator and will be used to call the function
+	that this is used on three times. Here is a basic example below.
+	"""
 	def wrapper(*args, **kwargs):
+		"""
+		This is the wrapper function that the decorator decorates.
+		"""
 		func(*args, **kwargs)
 		func(*args, **kwargs)
 		func(*args, **kwargs)
 	return wrapper
 @tripler
 def say_whee():
+	"""
+	Decorator operates on this function.
+	"""
 	print('Whee')
 
